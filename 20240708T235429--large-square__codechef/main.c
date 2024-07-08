b8d095a770eb25d93bab1509f7e95e491f2135ed
Anton Hakansson <anton.hakansson98@gmail.com>
2024-07-09 00:25:20 +0200

    sol: Large Square

---
+++ 20240708T235429--large-square__codechef/main.c
@@ -0,0 +1,109 @@
+/*
+#+title: Large Square
+#+ref: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/XLSQUARE
+#+author: anton@hakanssn.com
+#+licence: This is free and unencumbered software released into the public domain.
+ */
+
+#define count_of(s)  (Size)(sizeof((s)) / sizeof(*(s)))
+#define assert(c)    while((!(c))) __builtin_unreachable()
+#define new(a, t, n) ((t *) arena_alloc(a, (Size)sizeof(t), (Size)_Alignof(t), (n)))
+
+typedef unsigned char       U8;
+typedef unsigned long long  U64;
+typedef signed   long long  I64;
+typedef typeof((char *)0-(char *)0)  Size;
+typedef typeof((sizeof(0)))          USize;
+typedef struct { U8 *beg, *end; } Arena;
+typedef struct Stream { U8 *beg, *end, *at; void (*update)(struct Stream *); } Stream;
+
+static U8    *arena_alloc(Arena *a, Size objsize, Size align, Size count);
+static I64    read_i64   (Stream *io);
+static void   write_i64  (Stream *io, I64 x, char separator);
+
+static void run(Arena arena, Stream *reader, Stream *writer) {
+  Size T = read_i64(reader);
+  for (Size t = 0; t < T; t++) {
+    Size N = read_i64(reader);
+    Size A = read_i64(reader);
+    Size root = __builtin_sqrt(N);
+    Size result = (root < 1 ? 1 : root) * A;
+    write_i64(writer, result, '\n');
+  }
+}
+
+__attribute((malloc, alloc_size(2,4), alloc_align(3)))
+static U8 *arena_alloc(Arena *a, Size objsize, Size align, Size count) {
+  Size padding = -(USize)(a->beg) & (align - 1);
+  Size total   = padding + objsize * count;
+  assert(total < (a->end - a->beg) && "out of memory");
+  U8 *p = a->beg + padding;
+  __builtin_memset(p, 0, objsize * count);
+  a->beg += total;
+  return p;
+}
+static I64 read_i64(Stream *io) {
+  if (io->update && io->at + 256 >= io->end) { io->update(io); }
+  U64 u = 0, s = 0;
+  while(*io->at && *io->at <= 32) { ++io->at; }
+  if     (*io->at == '-') { s = ~s; ++io->at; } else if (*io->at == '+') { ++io->at; }
+  while ((*io->at >= '0') && (*io->at <= '9')) { u = u * 10 + (*(io->at++) - '0'); }
+  assert(io->at < io->end);
+  return (u^s) + !!s;
+}
+static void write_i64(Stream *io, I64 x, char separator) {
+  if (io->update && io->at + 256 >= io->end) { io->update(io); }
+  if (x < 0) { *io->at++ = '-'; x = -x; }
+  char tmpbuf[20];
+  int i = count_of(tmpbuf);
+  do { tmpbuf[--i] = (x % 10) + '0'; } while (x /= 10);
+  do { *io->at++ = tmpbuf[i++]; } while (i < count_of(tmpbuf));
+  *(io->at++) = separator;
+  assert(io->at < io->end);
+}
+
+// Platform
+#include <unistd.h>
+#include <stdlib.h>
+
+#define HEAP_CAP     (1ll << 30)
+
+static Stream stream(Arena *arena, Size cap, void (*update_fn)(Stream *)) {
+  Stream r = { .update = update_fn };
+  r.beg = r.at = new(arena, U8, cap);
+  r.end = r.beg + cap;
+  return r;
+}
+static void flush(Stream *s) {
+  assert(s->at >= s->beg);
+  Size nbytes = write(1, s->beg, s->at - s->beg);
+  s->at = s->beg;
+}
+static void fill(Stream *s) {
+  assert(s->end > s->at);
+  Size cap = s->end - s->beg;
+  Size unprocessed = (s->at == s->beg) ? 0 : (s->end - s->at);
+  __builtin_memmove(s->beg, s->at, unprocessed);
+  s->at = s->beg;
+  int nbytes = read(0, s->beg + unprocessed, cap - unprocessed);
+}
+
+#ifndef ONLINE_JUDGE
+#  include "test.c"
+#else
+int main(int argc, char **argv) {
+  void *heap = malloc(HEAP_CAP);
+  Arena arena = (Arena){heap, heap + HEAP_CAP};
+  Stream reader = stream(&arena, (1ll << 18), fill);
+  Stream writer = stream(&arena, (1ll << 18), flush);
+  fill(&reader);
+  run(arena, &reader, &writer);
+  flush(&writer);
+  free(heap);
+  return 0;
+}
+#endif // ONLINE_JUDGE
+
+/* Local Variables: */
+/* compile-command: "cc main.c -fsanitize=undefined -g3" */
+/* End: */
