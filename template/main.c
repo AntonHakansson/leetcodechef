0115bd94b894ad89f1a137701f5462c16461680e
Anton Hakansson <anton.hakansson98@gmail.com>
2024-09-23 19:10:52 +0200

    refactor: minimize template

--- template/main.c
+++ template/main.c
@@ -2,12 +2,14 @@
 #+title:
 #+ref:
 #+author: anton@hakanssn.com
-#+licence: This is free and unencumbered software released into the public domain.
- */
+#+licence: This is free and unencumbered software released into the public
+domain.
+*/
 
-#define count_of(s)  (Size)(sizeof((s)) / sizeof(*(s)))
-#define assert(c)    while((!(c))) __builtin_unreachable()
-#define new(a, t, n) ((t *) arena_alloc(a, (Size)sizeof(t), (Size)_Alignof(t), (n)))
+#define countof(s)    (Size)(sizeof((s)) / sizeof(*(s)))
+#define assert(c)     while((!(c))) __builtin_trap()
+#define breakpoint(c) ((c) ? ({ asm volatile ("int3; nop"); }) : 0)
+#define new(a, t, n)  ((t *) arena_alloc(a, (Size)sizeof(t), (Size)_Alignof(t), (n)))
 
 typedef unsigned char       U8;
 typedef unsigned long long  U64;
@@ -23,6 +25,7 @@ static void   write_i64  (Stream *io, I64 x, char separator);
 
 static void run(Arena arena, Stream *reader, Stream *writer) {
   Size T = read_i64(reader);
+  write_i64(writer, T, '\n');
 }
 
 __attribute((malloc, alloc_size(2,4), alloc_align(3)))
@@ -48,9 +51,9 @@ static void write_i64(Stream *io, I64 x, char separator) {
   if (io->update && io->at + 256 >= io->end) { io->update(io); }
   if (x < 0) { *io->at++ = '-'; x = -x; }
   char tmpbuf[20];
-  int i = count_of(tmpbuf);
-  do { tmpbuf[--i] = (x % 10) + '0'; } while (x /= 10);
-  do { *io->at++ = tmpbuf[i++]; } while (i < count_of(tmpbuf));
+  int i = countof(tmpbuf);
+  do { tmpbuf[--i] = (char)(x % 10) + '0'; } while (x /= 10);
+  do { *io->at++ = tmpbuf[i++]; } while (i < countof(tmpbuf));
   *(io->at++) = separator;
   assert(io->at < io->end);
 }
@@ -59,7 +62,6 @@ static void write_i64(Stream *io, I64 x, char separator) {
 #include <unistd.h>
 #include <stdlib.h>
 
-#define HEAP_CAP     (1ll << 30)
 
 static Stream stream(Arena *arena, Size cap, void (*update_fn)(Stream *)) {
   Stream r = { .update = update_fn };
@@ -70,21 +72,21 @@ static Stream stream(Arena *arena, Size cap, void (*update_fn)(Stream *)) {
 static void flush(Stream *s) {
   assert(s->at >= s->beg);
   Size nbytes = write(1, s->beg, s->at - s->beg);
+  assert(nbytes == (s->at - s->beg));
   s->at = s->beg;
 }
 static void fill(Stream *s) {
   assert(s->end > s->at);
   Size cap = s->end - s->beg;
   Size unprocessed = (s->at == s->beg) ? 0 : (s->end - s->at);
-  __builtin_memmove(s->beg, s->at, unprocessed);
-  s->at = s->beg;
-  int nbytes = read(0, s->beg + unprocessed, cap - unprocessed);
+  s->at = __builtin_memmove(s->beg, s->at, unprocessed);
+  I64 nbytes = read(0, s->beg + unprocessed, cap - unprocessed);
+  assert(nbytes == (cap - unprocessed));
 }
 
-#ifndef ONLINE_JUDGE
-#  include "test.c"
-#else
+#ifdef ONLINE_JUDGE
 int main(int argc, char **argv) {
+  enum { HEAP_CAP = 1ll << 30 };
   void *heap = malloc(HEAP_CAP);
   Arena arena = (Arena){heap, heap + HEAP_CAP};
   Stream reader = stream(&arena, (1ll << 18), fill);
@@ -92,11 +94,6 @@ int main(int argc, char **argv) {
   fill(&reader);
   run(arena, &reader, &writer);
   flush(&writer);
-  free(heap);
   return 0;
 }
 #endif // ONLINE_JUDGE
-
-/* Local Variables: */
-/* compile-command: "cc main.c -fsanitize=undefined -g3" */
-/* End: */
--- template/test.c
+++ template/test.c
@@ -1,36 +1,38 @@
-static struct { char *in; char *out; } test_cases[] = {
+#if IN_SHELL /* $ bash test.c
+cc test.c -o test -fsanitize=undefined -g3 -O0 -Wall -Wextra -Wconversion -Wno-sign-conversion $@
+exit # */
+#endif
+
+#include "main.c"
+
+static struct {
+  char *in;
+  char *out;
+}
+test_cases[] = {
   {
-    .in = "\n",
-    .out = "\n",
+    .in  = "",
+    .out = "",
   },
 };
 
-static void pprint2D(Stream *writer, U64 *a, Size count, Size stride) {
-  for (Size row = 0; row < count; row++) {
-    for (Size col = 0; col < stride; col++) {
-      write_i64(writer, a[row * stride + col], ' ');
-    }
-    *(writer->at++) = '\n';
-  }
-  *(writer->at++) = '\n';
-}
-
 #include <stdio.h>
-int main(int argc, char **argv)
-{
+int main() {
+  enum { HEAP_CAP = 1ll << 30 };
   void *heap = malloc(HEAP_CAP);
   Arena arena = (Arena){heap, heap + HEAP_CAP};
-  for (Size i = 0; i < count_of(test_cases); i++) {
+  for (Size i = 0; i < countof(test_cases); i++) {
+    if (__builtin_strlen(test_cases[i].in) == 0) continue;
     Arena temp = arena;
     Stream reader = {0}; {
-      reader.beg = reader.at = test_cases[i].in;
+      reader.beg = reader.at = (U8 *)test_cases[i].in;
       reader.end = reader.beg + __builtin_strlen(test_cases[i].in);
     }
     Stream writer = stream(&temp, (1ll << 13), flush);
     run(temp, &reader, &writer);
     flush(&writer);
-    if (__builtin_strcmp(writer.beg, test_cases[i].out) != 0) {
-      fprintf(stderr, "==Failed case %d==\n%sExpected: %sGot: %s", i, test_cases[i].in, test_cases[i].out, writer.beg);
+    if (__builtin_strcmp((char *)writer.beg, test_cases[i].out) != 0) {
+      fprintf(stderr, "==Failed case %ld==\n%sExpected: %sGot: %s", i, test_cases[i].in, test_cases[i].out, writer.beg);
     }
   }
   free(heap);
