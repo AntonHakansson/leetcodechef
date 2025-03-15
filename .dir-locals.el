2b481a021aafb6b88b4968925cd9bfd6e58509c7
Anton Hakansson <anton.hakansson98@gmail.com>
2025-03-15 15:04:28 +0100

    feat(repo): elisp helper for copying template

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (defun hk/leetcodechef-copy (name)
+                   (interactive "sEnter name: ")
+                   (copy-directory "template" (denote-format-file-name "./" (denote-get-identifier) nil name nil nil)))))))
